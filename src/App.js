5e5b1b54e7f4cf40ab963fc4093abcbda8c44990
jilvin <jilvinjacob@gmail.com>
2018-06-18 11:56:33 +0530

    new-design init

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-
 import React from 'react';
-
-import GoldenContainer from './components/GoldenContainer.jsx'
 
 import './App.css';
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <GoldenContainer />
+      <div className="App">
+        <div id="header">
+          <h1 id="institute-name">College of Engineering</h1>
+          <h2 id="institute-name-sub">Trivandrum</h2>
+        </div>
       </div>
     );
   }
