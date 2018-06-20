1f5db9ffc5f57b772b8fb35a6c39676b20c69e4a
robin <robinl3680@gmail.com>
2018-06-20 16:09:20 +0530

    Link for navbar

--- src/components/StaticUpperNav/InstitutionName.jsx
+++ src/components/StaticUpperNav/InstitutionName.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 import './institution-name.css'
 class InstitutionName extends React.Component {
 render() {
     return <div id="wrapper">
-        <div id="name"> College of Engineering </div>
-        <div id="place">Trivandrum</div>         
+       <Link to="/" id="Link-color"> <div id="name">College of Engineering  </div>
+        <div id="place">Trivandrum</div> </Link>       
     </div>
 }
 }
