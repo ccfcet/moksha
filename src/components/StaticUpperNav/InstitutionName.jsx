1687ec1b48ce1e988abd7a7bbfd1fa07bc96dcad
Santhisenan <santhisenan@outlook.com>
2018-06-20 15:06:37 +0530

    minor change in nav

--- src/components/StaticUpperNav/InstitutionName.jsx
+++ src/components/StaticUpperNav/InstitutionName.jsx
@@ -4,12 +4,8 @@ import './institution-name.css'
 class InstitutionName extends React.Component {
 render() {
     return <div id="wrapper">
-        {/* <span id="name"> College of Engineering </span>
-        <span id="place">Trivandrum</span>          */}
-        <li>
-            <ol id="name-ol"><span id="name"> College of Engineering </span></ol>
-            <ol id="place-ol"><span id="place">Trivandrum</span></ol>
-        </li>
+        <div id="name"> College of Engineering </div>
+        <div id="place">Trivandrum</div>         
     </div>
 }
 }
