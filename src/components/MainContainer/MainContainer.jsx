1ed8e202c1c3a69915152493b0d9de255e7374d6
jilvin <jilvinjacob@gmail.com>
2018-06-25 15:18:29 +0530

    Make landing page responsive for various devices in their default rotation position

--- src/components/MainContainer/MainContainer.jsx
+++ src/components/MainContainer/MainContainer.jsx
@@ -6,21 +6,15 @@ import { Link } from 'react-router-dom';
 
 import './main-container.css';
 
-
 class MainContainer extends React.Component {
-
   render() {
     return (
-
       <div className="main-container">
         <div id="container">
           <div id="header">
             <h1 id="institute-name">College of Engineering</h1>
             <h2 id="institute-name-sub">Trivandrum</h2>
-
           </div>
-
-
           <div id="main-menu">
             <Menu
               mode="horizontal"
@@ -30,19 +24,19 @@ class MainContainer extends React.Component {
                   <Link to="/about">About</Link>
                 </Menu.Item>
                 <Menu.Item>
-                <Link to="/administration">Administration</Link>
+                  <Link to="/administration">Administration</Link>
                 </Menu.Item>
                 <Menu.Item>
-                <Link to="/academics">Academics</Link>
+                  <Link to="/academics">Academics</Link>
                 </Menu.Item>
                 <Menu.Item>
-                <Link to="/activities">Activities</Link>
+                  <Link to="/activities">Activities</Link>
                 </Menu.Item>
                 <Menu.Item>
-                <Link to="/facilities">Facilities</Link>
+                  <Link to="/facilities">Facilities</Link>
                 </Menu.Item>
                 <Menu.Item>
-                <Link to="/more">More</Link>
+                  <Link to="/more">More</Link>
                 </Menu.Item>
                 {/* <Menu.Item>
                   <a href="https://ant.design" target="_blank" rel="noopener noreferrer">Navigation Four - Link</a>
