838ad143b906e2c7439f65ab4b401fcd36ce8ad8
jilvin <jilvinjacob@gmail.com>
2018-07-13 21:42:25 +0530

    Add material ui

--- src/next/components/simple/menu_bar/index.jsx
+++ src/next/components/simple/menu_bar/index.jsx
@@ -4,15 +4,15 @@ class MenuBar extends React.Component {
   render() {
     return (
       <div id="menu-bar">
-          <ul id="menu-bar-list">
-            <li><a href="#home">Home</a></li>
-            <li><a href="#news">News</a></li>
-            <li><a href="#contact">Contact</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#home">Home</a></li>
-            <li><a href="#news">News</a></li>
-            <li><a href="#contact">Contact</a></li>
-          </ul>
+        <ul id="menu-bar-list">
+          <li><a href="#home">Home</a></li>
+          <li><a href="#news">News</a></li>
+          <li><a href="#contact">Contact</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#home">Home</a></li>
+          <li><a href="#news">News</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
         <style jsx>
           {`
             @import url('https://fonts.googleapis.com/css?family=Raleway');
