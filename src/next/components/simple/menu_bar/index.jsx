f84e6199cfebf48d0072eeb9d606a5303979b97b
jilvin <jilvinjacob@gmail.com>
2018-07-21 11:51:53 +0530

    Add menu responsive -- bug in home page --fix soon

--- src/next/components/simple/menu_bar/index.jsx
+++ src/next/components/simple/menu_bar/index.jsx
@@ -1,66 +1,47 @@
 import React from 'react';
+import ResponsiveMenu from 'react-responsive-navbar'
+
+import './style.css'
 
 class MenuBar extends React.Component {
   render() {
     return (
-      <div id="menu-bar">
-        <ul id="menu-bar-list">
-          <li><a href="/">Home</a></li>
-          <li><a href="/about">About</a></li>
-          <li><a href="/academics">Academics</a></li>
-          <li><a href="/activities">Activities</a></li>
-          <li><a href="/departments">Departments</a></li>
-          <li><a href="/facilities">Facilities</a></li>
-          <li><a href="/contact">Contact</a></li>
-        </ul>
-        <style jsx>
-          {`
-            @import url('https://fonts.googleapis.com/css?family=Raleway');
-
-            #menu-bar {
-              margin: 0 auto;
-              margin-top: 0px;
-              text-align:center;
-              height: 100%;
+      <div>
+        <ResponsiveMenu
+          className="menu-tag"
+          menuOpenButton={<div id="menu-open-text">
+            Menu
+            </div>}
+            menuCloseButton={<div id="menu-close-text">Close</div>}
+            changeMenuOn="660px"
+            largeMenuClassName="large-menu"
+            smallMenuClassName="small-menu"
+            menu={
+              <div id="menu-bar">
+                <ul id="menu-bar-list">
+                  <li><a href="/">Home</a></li>
+                  <li><a href="/about">About</a></li>
+                  <li><a href="/academics">Academics</a></li>
+                  <li><a href="/activities">Activities</a></li>
+                  <li><a href="/departments">Departments</a></li>
+                  <li><a href="/facilities">Facilities</a></li>
+                  <li><a href="/contact">Contact</a></li>
+                </ul>
+              </div>
             }
-
-            #menu-bar-list {
-              display: table;
-              margin: auto;
-              padding-left:10px;
-              padding-right:10px;
-              height: 100%;
-            }
-
-            #menu-bar-list li {
-              display: table-cell;
-              vertical-align: middle;
-              // height: 100%;
-              // display: inline-block;
-              // display: table;
-              list-style-type:none;
-              // margin: 2px;
-              padding-left: 20px;
-              padding-right: 20px;
-            }
-
-            #menu-bar-list li a{
-              color:#5d5d5d;
-              text-decoration: none;
-              display: inline-block;
-              // display: table-cell;
-              // vertical-align: middle;
+          />
+          <style jsx>{`
+            #menu-open-text, #menu-close-text {
+              color: #5d5d5d;
               font-family: 'Open Sans',sans-serif;
-              text-align: center;
               font-weight: 300;
               font-size: 2.6vh;
-              margin-bottom: 0.4vh;
+              margin-bottom: 0.2vh;
             }
-            `}
-          </style>
-        </div>
-      );
+            `}</style>
+          </div>
+        );
+      }
     }
-  }
 
-  export default MenuBar;
+    export default MenuBar;
