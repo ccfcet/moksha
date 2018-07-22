6a1cebc451927607c6e0390f2b122ba2c128f772
Samanyu13 <samanyusaji18@gmail.com>
2018-07-22 11:16:45 +0530

    Need to improve academics page

--- src/next/components/content/academic_page/index.jsx
+++ src/next/components/content/academic_page/index.jsx
@@ -2,58 +2,86 @@ import React from 'react'
 // import Grid from '@material-ui/core/Grid';
 import DepartmentCard from '../../simple/department_cards'
 import {Container,Row,Col} from 'reactstrap'
+import { Button } from 'reactstrap';
+
 class departmentPage extends React.Component{
     render(){
         return(
             <div style={{height:'100%'}}>
               <Container fluid={true}>
-                <Row>
-                  <Col>
-                    <div id='department-page-title'>
-                      Academics
+                <Row >
+                  <Col xs="6">
+                    <div id="ug-img">
+                      <img src='../../../static/images/12.jpg'/><br/>
+                      Undergraduate Courses(should be over the img)
                     </div>
-                  </Col>  
-                </Row>
-                  <div id='department-page-content'>
-                  <Row>
-                    
-                
-                      <Col sm={{ size: 'auto', offset: 2}} >
-                        
-                            <DepartmentCard departmentName='Undergraduate Studies' image='/static/icons/ug.png' departmentURL='/ug'/>
-                        
-                      </Col>
-                      <Col sm={{ size: 'auto', offset: 2 }}>    
-                        
-                            <DepartmentCard departmentName='Postgraduate Studies' image='/static/icons/pg.png' departmentURL='/pg'/>
-                        
-                      </Col>    
-                
-              </Row>    
+                  </Col>
 
-                </div>
+                  <Col xs="6"  style={{'background-color' :'#5265ad'}}>
+                    <div id="ug-content">
+An undergraduate degree (also called first degree, bachelor's degree or simply
+degree) is a colloquial term for an academic degree taken by a person who has
+completed undergraduate courses. It is usually offered at an institution of
+higher education, such as a university.<br/>
+<Button color="danger">Learn More</Button>{' '}
+                    </div>
+                  </Col>
+                </Row>
+
+                <Row>
+                  <Col xs="6" style={{'background-color' :'#5265ad'}}>
+                    <div id="pg-content">
+Postgraduate education, or graduate education in North America, involves
+learning and studying for academic or professional degrees, academic or
+professional certificates, academic or professional diplomas, or other
+qualifications for which a first or bachelor's degree generally is required,
+and it is normally considered to be part of higher education.
+<br/>
+                      <Button color="danger">Learn More</Button>{' '}
+                    </div>
+                  </Col>
+                  <Col xs="6">
+                    <div id="pg-img">
+                      <img src='../../../static/images/13.jpg'/><br/>
+                      Postgraduate Courses(should be over the img)
+                    </div>
+                  </Col>
+                </Row>
 
               <style jsx>
                 {`
-                  #department-page-title {
-                    height : 100%;
-                    padding-top : 50px;
+                  #ug-img {
                     text-align : center;
-                    background-color : #0000e8;
-                    font-size : 50px;
+                    height : 40vh;
+                  }
+                  #ug-content {
+                    height : 40vh;
+                    padding-left : 20px;
+                    padding-right : 20px;
+                    padding-top : 40px;
                     color : white;
-                    padding-bottom: 50px;
-                    width:100%;
+                    font-size : 1.3em;
                   }
 
-                  #department-page-content {
-                    padding-top:20px;
-                    padding-bottom:20px;
+                  #pg-img {
+                    text-align : center;
+                    height : 40vh;
                   }
 
-                  #row {
-                    padding-bottom:32px;
+                  #pg-content {
+                    height 40vh;
+                    color : white;
+                    font-size : 1.3em;
+                    padding-left : 20px;
+                    padding-right : 20px;
+                    padding-top : 40px;
                   }
+
+                  img {
+                    width: 30vw;
+                    padding : 20px;
+                  }
+
                 `}
               </style>
               </Container>
