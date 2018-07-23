89aa8a0503e80b6f3d5704d4241d60be3d81b7f7
Samanyu13 <samanyusaji18@gmail.com>
2018-07-23 12:04:08 +0530

    Small changes to Academics page

--- src/next/components/content/academic_page/index.jsx
+++ src/next/components/content/academic_page/index.jsx
@@ -10,40 +10,41 @@ class departmentPage extends React.Component{
             <div style={{height:'100%'}}>
               <Container fluid={true}>
                 <Row >
-                  <Col xs="6">
+                  <Col sm="6">
                     <div id="ug-img">
                       <img src='../../../static/images/12.jpg'/><br/>
-                      Undergraduate Courses(should be over the img)
+
                     </div>
                   </Col>
 
-                  <Col xs="6"  style={{'background-color' :'#5265ad'}}>
+                  <Col sm="6"  style={{'background-color' :'#5265ad'}}>
                     <div id="ug-content">
-An undergraduate degree (also called first degree, bachelor's degree or simply
+                      <h2>Undergraduate Courses</h2>
+<p>An undergraduate degree (also called first degree, bachelor's degree or simply
 degree) is a colloquial term for an academic degree taken by a person who has
 completed undergraduate courses. It is usually offered at an institution of
-higher education, such as a university.<br/>
-<Button color="danger">Learn More</Button>{' '}
+higher education, such as a university.</p><br/>
+<p><Button color="danger">Learn More</Button>{' '}</p>
                     </div>
                   </Col>
                 </Row>
 
                 <Row>
-                  <Col xs="6" style={{'background-color' :'#5265ad'}}>
+                  <Col sm="6" style={{'background-color' :'#5265ad'}}>
                     <div id="pg-content">
-Postgraduate education, or graduate education in North America, involves
+                    <h2>Postgraduate Courses</h2>
+<p>ostgraduate education, or graduate education in North America, involves
 learning and studying for academic or professional degrees, academic or
 professional certificates, academic or professional diplomas, or other
 qualifications for which a first or bachelor's degree generally is required,
-and it is normally considered to be part of higher education.
+and it is normally considered to be part of higher education.</p>
 <br/>
-                      <Button color="danger">Learn More</Button>{' '}
+<p><Button color="danger">Learn More</Button>{' '}</p>
                     </div>
                   </Col>
-                  <Col xs="6">
+                  <Col sm="6">
                     <div id="pg-img">
                       <img src='../../../static/images/13.jpg'/><br/>
-                      Postgraduate Courses(should be over the img)
                     </div>
                   </Col>
                 </Row>
@@ -52,10 +53,9 @@ and it is normally considered to be part of higher education.
                 {`
                   #ug-img {
                     text-align : center;
-                    height : 40vh;
+                    // height : 40vh;
                   }
                   #ug-content {
-                    height : 40vh;
                     padding-left : 20px;
                     padding-right : 20px;
                     padding-top : 40px;
@@ -65,11 +65,9 @@ and it is normally considered to be part of higher education.
 
                   #pg-img {
                     text-align : center;
-                    height : 40vh;
                   }
 
                   #pg-content {
-                    height 40vh;
                     color : white;
                     font-size : 1.3em;
                     padding-left : 20px;
@@ -78,7 +76,8 @@ and it is normally considered to be part of higher education.
                   }
 
                   img {
-                    width: 30vw;
+                    width: 100%;
+                    height : 40vh;
                     padding : 20px;
                   }
 
