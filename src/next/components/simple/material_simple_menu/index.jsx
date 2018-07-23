5b7c2fcd1b0e8029ec2a58f6d37401b6de7e810b
jilvin <jilvinjacob@gmail.com>
2018-07-23 13:14:52 +0530

    Add support for drop down menu

---
+++ src/next/components/simple/material_simple_menu/index.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+
+class SimpleMenu extends React.Component {
+  state = {
+    anchorEl: null,
+  };
+
+  handleClick = event => {
+    this.setState({ anchorEl: event.currentTarget });
+  };
+
+  handleClose = () => {
+    this.setState({ anchorEl: null });
+  };
+
+  render() {
+    const { anchorEl } = this.state;
+
+    return (
+      <div>
+        <Button
+          aria-owns={anchorEl ? 'simple-menu' : null}
+          aria-haspopup="true"
+          onClick={this.handleClick}
+          style={{padding: '0',
+          textTransform: 'none',
+          fontFamily: '\'Open Sans\',sans-serif',
+          fontWeight: '300',
+          fontSize: '2.6vh',
+          color: '#5d5d5d',
+          minHeight: 'unset'}}
+          >
+            {this.props.menuTitle}
+          </Button>
+          <Menu
+            id="simple-menu"
+            anchorEl={anchorEl}
+            open={Boolean(anchorEl)}
+            onClose={this.handleClose}
+            >
+              {this.props.menuElements.map(menuElement => (
+                <a href={menuElement.url}>
+                  <MenuItem>
+                    {menuElement.name}
+                  </MenuItem>
+                </a>
+              ))}
+            </Menu>
+          </div>
+        );
+      }
+    }
+
+    export default SimpleMenu;
--- src/next/components/simple/menu_bar/index.jsx
+++ src/next/components/simple/menu_bar/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ResponsiveMenu from 'react-responsive-navbar'
 
 import './style.css'
+import MaterialSimpleMenu from '../material_simple_menu';
 
 class MenuBar extends React.Component {
   render() {
@@ -11,37 +12,103 @@ class MenuBar extends React.Component {
           className="menu-tag"
           menuOpenButton={<div id="menu-open-text">
             Menu
-            </div>}
-            menuCloseButton={<div id="menu-close-text">Close</div>}
-            changeMenuOn="660px"
-            largeMenuClassName="large-menu"
-            smallMenuClassName="small-menu"
-            menu={
-              <div id="menu-bar">
-                <ul id="menu-bar-list">
-                  <li><a href="/">Home</a></li>
-                  <li><a href="/about">About</a></li>
-                  <li><a href="/academics">Academics</a></li>
-                  <li><a href="/activities">Activities</a></li>
-                  <li><a href="/departments">Departments</a></li>
-                  <li><a href="/facilities">Facilities</a></li>
-                  <li><a href="/contact">Contact</a></li>
-                </ul>
-              </div>
+          </div>}
+          menuCloseButton={<div id="menu-close-text">Close</div>}
+          changeMenuOn="660px"
+          largeMenuClassName="large-menu"
+          smallMenuClassName="small-menu"
+          menu={
+            <div id="menu-bar">
+              <ul id="menu-bar-list">
+                <li><a href="/">Home</a></li>
+                <li><MaterialSimpleMenu menuTitle="About"
+                  menuElements={[
+                    {
+                      "name": "College",
+                      "url": "about/college"
+                    },
+                    {
+                      "name": "Mission and Vision",
+                      "url": "about/mission_and_vision"
+                    },
+                    {
+                      "name": "Route Map",
+                      "url": "about/route_map"
+                    },
+                    {
+                      "name": "Contact",
+                      "url": "about/contact"
+                    }
+                  ]}/></li>
+                  <li><MaterialSimpleMenu menuTitle="Departments"
+                    menuElements={[
+                      {
+                        "name": "Civil Engineering",
+                        "url": "department/civil_engineering"
+                      },
+                      {
+                        "name": "Mechanical Engineering",
+                        "url": "department/mechanical_engineering"
+                      },
+                      {
+                        "name": "Electrical and Electronics Engineering",
+                        "url": "department/civil_engineering"
+                      },
+                      {
+                        "name": "Electronics and Communication Engineering",
+                        "url": "department/civil_engineering"
+                      },
+                    ]}/></li>
+                    <li><MaterialSimpleMenu menuTitle="Activities"
+                      menuElements={[
+                        {
+                          "name": "College Union",
+                          "url": "department/civil_engineering"
+                        },
+                        {
+                          "name": "Clubs",
+                          "url": "activities/clubs"
+                        },
+                      ]}/></li>
+                      <li><MaterialSimpleMenu menuTitle="Academics"
+                        menuElements={[
+                          {
+                            "name": "Programmes",
+                            "url": "academics/programmes"
+                          },
+                          {
+                            "name": "People",
+                            "url": "academics/people"
+                          },
+                        ]}/></li>
+                        <li><MaterialSimpleMenu menuTitle="Facilities"
+                          menuElements={[
+                            {
+                              "name": "Programmes",
+                              "url": "academics/programmes"
+                            },
+                            {
+                              "name": "People",
+                              "url": "academics/people"
+                            },
+                          ]}/></li>
+                          <li><a href="/search">Search</a></li>
+                        </ul>
+                      </div>
+                    }
+                  />
+                  <style jsx>{`
+                    #menu-open-text, #menu-close-text {
+                      color: #5d5d5d;
+                      font-family: 'Open Sans',sans-serif;
+                      font-weight: 300;
+                      font-size: 2.6vh;
+                      margin-bottom: 0.2vh;
+                    }
+                    `}</style>
+                  </div>
+                );
+              }
             }
-          />
-          <style jsx>{`
-            #menu-open-text, #menu-close-text {
-              color: #5d5d5d;
-              font-family: 'Open Sans',sans-serif;
-              font-weight: 300;
-              font-size: 2.6vh;
-              margin-bottom: 0.2vh;
-            }
-            `}</style>
-          </div>
-        );
-      }
-    }
 
-    export default MenuBar;
+            export default MenuBar;
