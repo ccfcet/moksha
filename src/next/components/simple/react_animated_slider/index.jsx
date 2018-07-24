79ea8c59ee2a5e567e90003142da3aaee63b78ce
Santhisenan <santhisenan@outlook.com>
2018-07-24 13:05:27 +0530

    added one more slider

--- src/next/components/simple/react_animated_slider/index.jsx
+++ src/next/components/simple/react_animated_slider/index.jsx
@@ -13,6 +13,14 @@ const content = [
     button: 'Discover',
     image: './static/images/hwwe_2018.jpg',
   },
+  {
+    id: 2,
+    title: 'Driving Innovation',
+    description:
+    'The 16th International Conference on Control, Communication and Computing (IC4 2018) offers a platform for researchers, practitioners, manufacturers and academicians to discuss the ongoing research  across the world  and help exchange  ideas in addressing work related issues faced by human beings in all facets of daily activities.',
+    button: 'Discover',
+    image: './static/images/hwwe_2018.jpg',
+  }
 ];
 
 class ReactAnimatedSlider extends React.Component {
@@ -25,22 +33,22 @@ class ReactAnimatedSlider extends React.Component {
               <Slider
                 infinite="true"
                 autoplay="4000"
-                previousButton={null}
-                nextButton={null}
+                // previousButton={null}
+                // nextButton={null}
                 >
                   {content.map(article => (
                     <div
                       key={article.id}
                       className="slider-content"
                       style={{
-                        // background: `url('${article.image}') no-repeat center center`
+                        background: `url('${article.image}') no-repeat center center`
                         // background: '#0000e7',
                         /* Permalink - use to edit and share this gradient: http://colorzilla.com/gradient-editor/#0000e8+0,0000ff+100 */
-                        background: 'rgb(0,0,232)', /* Old browsers */
-                        background: '-moz-linear-gradient(top, rgba(0,0,232,1) 0%, rgba(0,0,255,1) 100%)', /* FF3.6-15 */
-                        background: '-webkit-linear-gradient(top, rgba(0,0,232,1) 0%,rgba(0,0,255,1) 100%)', /* Chrome10-25,Safari5.1-6 */
-                        background: 'linear-gradient(to bottom, rgba(0,0,232,1) 0%,rgba(0,0,255,1) 100%)', /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
-                        filter: 'progid:DXImageTransform.Microsoft.gradient( startColorstr=\'#0000e8\', endColorstr=\'#0000ff\',GradientType=0 )', /* IE6-9 */
+                        // background: 'rgb(0,0,232)', /* Old browsers */
+                        // background: '-moz-linear-gradient(top, rgba(0,0,232,1) 0%, rgba(0,0,255,1) 100%)', /* FF3.6-15 */
+                        // background: '-webkit-linear-gradient(top, rgba(0,0,232,1) 0%,rgba(0,0,255,1) 100%)', /* Chrome10-25,Safari5.1-6 */
+                        // background: 'linear-gradient(to bottom, rgba(0,0,232,1) 0%,rgba(0,0,255,1) 100%)', /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
+                        // filter: 'progid:DXImageTransform.Microsoft.gradient( startColorstr=\'#0000e8\', endColorstr=\'#0000ff\',GradientType=0 )', /* IE6-9 */
                       }}
                       >
                         <div className="inner">
