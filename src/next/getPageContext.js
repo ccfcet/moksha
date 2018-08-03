c96fe4d67ae77045446224b0b02f4e7ef8e1c750
Santhisenan <santhisenan@outlook.com>
2018-08-03 06:55:00 +0530

    Fix all the linter errors

--- src/next/getPageContext.js
+++ src/next/getPageContext.js
@@ -1,8 +1,8 @@
 /* eslint-disable no-underscore-dangle */
 
 import { SheetsRegistry } from 'jss';
-import { createMuiTheme } from '@material-ui/core/styles';
-import { createGenerateClassName } from '@material-ui/core/styles';
+import { createMuiTheme, createGenerateClassName } from
+  '@material-ui/core/styles';
 
 // A theme with custom primary and secondary color.
 // It's optional.
